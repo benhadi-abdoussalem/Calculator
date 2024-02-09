2b1cf73738d3647c14de2ec94133698c7f340ca3
benhadi <dr.programme@gmail.com>
2024-02-09 22:44:38 +0100

    09/02/2024

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-class calculator {
+class Calculator {
     constructor (previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
@@ -22,7 +22,7 @@ class calculator {
 
     }
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
+    this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
 
@@ -30,9 +30,10 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const allclearButton = document.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
