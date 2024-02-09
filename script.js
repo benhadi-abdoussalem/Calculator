fed7939becefec89c0fc2921ed2c2d518819aa04
benhadi <dr.programme@gmail.com>
2024-02-09 22:28:09 +0100

    09/02/2024

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-class Calculator {
+class calculator {
     constructor (previousOperandTextElement, currentOperandTextElement){
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
     clear(){
-
-    }
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+        }
     delete(){
 
     }
     appendNumber(number){
-
+        this.currentOperand = number
     }
     chooseOperation(operation){
 
@@ -19,13 +22,9 @@ class Calculator {
 
     }
     updateDisplay(){
-
+        this.currentOperandTextElement.innerText = this.currentOperand
     }
 }
-
-
-
-
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -34,3 +33,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allclearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
