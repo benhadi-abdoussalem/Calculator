ca6cca1a8c232d4dea540967ec621b791b9c592e
benhadi <dr.programme@gmail.com>
2024-02-12 22:19:28 +0100

    12/02/2024

--- script.js
+++ script.js
@@ -44,3 +44,9 @@ numberButtons.forEach((button) => {
     calculator.updateDisplay();
   });
 });
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
